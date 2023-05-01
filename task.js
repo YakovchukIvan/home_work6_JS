87a487d613aca3949ee4f0372bcdbf91bf50d884
litvinovjobs@gmail.com <litvinovjobs@gmail.com>
2023-05-01 17:41:32 +0300

    codding start task6

--- task.js
+++ task.js
@@ -1,1 +1,24 @@
 "use strict"
+
+const taskInput1 = document.querySelector(".taskInput1")
+const taskInput2 = document.querySelector(".taskInput2")
+
+const taskBtn1 = document.querySelector(".taskBtn1")
+const taskBtn2 = document.querySelector(".taskBtn2")
+
+
+
+
+function task1 () {
+
+    taskBtn1.addEventListener("click", () => {
+        console.log(taskInput1.value);
+        taskInput1.value = ""
+    })
+
+    taskBtn2.addEventListener("click", () => {
+        console.log(taskInput2.value);
+        taskInput2.value = ""
+    })
+}
+task1 ()
